6fdff5d37d6df0da45bb57756ca313a041305a5f
samiullah <sediqzada94@gmail.com>
2023-05-14 15:23:55 +0430

    Video comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
 import { useState } from 'react';
+import moment from 'moment/moment';
 const Comment = ({ comment }) => {
         const [showMore, setShowMore] = useState(false)
         const likeCount = comment?.snippet?.topLevelComment?.snippet?.likeCount;
         const commentText = comment?.snippet?.topLevelComment?.snippet?.textDisplay
   return (
-    <div className='flex flex-col shadow-sm p-4 my-1 rounded-lg bg-gray-50'>
-        <div className='flex mb-3'>
+    <div className='flex  items-start shadow-sm p-4 my-1 rounded-lg bg-gray-50 transition duration-300'>
             <div className='mr-4 w-20 flex items-center justify-center'>
                 <img className='rounded-full h-10 w-10' src={ comment?.snippet?.topLevelComment?.snippet?.authorProfileImageUrl } />
             </div>
-            <div className='flex flex-col'>
+            <div className='flex flex-col flex-1'>
               <span>
                  <span className='mr-5'>{ comment?.snippet?.topLevelComment?.snippet?.authorDisplayName }</span>
-                 <span>{ comment?.snippet?.topLevelComment?.snippet?.publishedAt }</span>
-                </span> 
-              <div className='text-sm mt-2 text-gray-600 leading-6'> { showMore ? commentText : commentText.slice(0, 100) }
+                 <span>{ moment(comment?.snippet?.topLevelComment?.snippet?.publishedAt, 'YYYYMMDD').fromNow() }</span>
+                </span>
+              <div className='text-sm mt-2 text-gray-600 leading-6' dangerouslySetInnerHTML={{ __html: showMore ? commentText : commentText.slice(0, 100) }}> 
+               </div>
               { commentText.length > 100 &&
                <button  className='text-blue-500 hover:text-blue-600 ml-1' onClick={ () => {setShowMore((prev) => !prev)}}>
                   { showMore ? 'See less' : <span className='text-md'>... <span className='ml-3'>See more</span> </span> }
                   </button>
                   }
-               </div>
+              <div className='pl-24 text-sm'>
+              { likeCount } {" "+ likeCount > 1 ? 'likes' : 'like'} 
+              </div>
             </div>
-        </div>
-        <div className='pl-24 text-sm'>
-        { likeCount } {" "+ likeCount > 1 ? 'likes' : 'like'} 
-        </div>
+      
 
     </div>
   )
