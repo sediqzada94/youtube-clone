3801815be04156dfd23baa1b4c6fbed053d3f3c5
samiullah <sediqzada94@gmail.com>
2023-06-05 15:47:34 +0430

    Error handling in home page

--- src/components/Core/Error.jsx
+++ src/components/Core/Error.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Error = () => {
   return (
-    <div>Something went wrong please try again later</div>
+    <div
+    className='fixed top-0 left-0 w-full h-screen z-50 flex items-center justify-center bg-white/75 dark:bg-white/20 font-semibold'
+    >
+      Something went wrong please try again later
+      </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,31 +2,46 @@ import { useState, useEffect } from 'react'
 import { Videos } from '../components'
 import { fetchApiData } from "../utils/fetchApiData";
 import { Loading,VideoCategoryLinks } from '../components'
+import Error from '../components/Core/Error';
 const Home = ({ selectedVideoCategory, setSelectedVideoCategory }) => {
   const [videos, setVideos] = useState([])
-  const [isLoading, setisLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
  
   useEffect( () => {
-    setisLoading(true)
-    fetchApiData(
-     `search?part=snippet&q=${selectedVideoCategory}`
-     ).then((data)=>{
-         setVideos(data.items)
-         setisLoading(false)
-     });
+    setIsLoading(true)
+    const fetchData = async () => {
+      try {
+        const data = await fetchApiData(`search?part=snippet&q=${selectedVideoCategory}`);
+        setVideos(data.items);
+        setIsLoading(false);
+      } catch (error) {
+        if (error?.response?.status === 429) {
+          setIsError(true);
+          setIsLoading(false);
+          console.log("Errorrrr:", error);
+        }
+      }
+    };
+  
+    fetchData();
     return () => {  
     }
   }, [selectedVideoCategory])
   return (
-    <div className=''>
-         <VideoCategoryLinks  selectedVideoCategory={selectedVideoCategory} setSelectedVideoCategory={setSelectedVideoCategory}/>
-        { videos.length !== 1 && <h1 className=' mx-24 my-6 text-xl font-semibold'>
-        <span className='px-2 py-1 rounded-md bg-gray-300 dark:bg-gray-500 capitalize'>{selectedVideoCategory.length > 0 ? selectedVideoCategory : 'All' }</span> videos
-        </h1>}
-        {isLoading ? <Loading /> :
-        <Videos videos={videos} setVideos = { setVideos } />
-        }
-    </div>
+    <>
+        {isError ? <Error /> :
+        <div className=''>
+            <VideoCategoryLinks  selectedVideoCategory={selectedVideoCategory} setSelectedVideoCategory={setSelectedVideoCategory}/>
+            { videos.length !== 1 && <h1 className=' mx-24 my-6 text-xl font-semibold'>
+            <span className='px-2 py-1 rounded-md bg-gray-300 dark:bg-gray-500 capitalize'>{selectedVideoCategory.length > 0 ? selectedVideoCategory : 'All' }</span> videos
+            </h1>}
+            {isLoading ? <Loading /> :
+            <Videos videos={videos} setVideos = { setVideos } />
+            }
+        </div>
+}
+    </>
   )
 }
 
